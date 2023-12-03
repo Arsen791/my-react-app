1f24db786f7cc56e5407982a6849c5b8810ac30f
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-12-03 08:48:09 +0600

    Code refactoring2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -53,7 +53,9 @@ export default class App extends Component{
                 <div className="col-md-6">
                     <ItemList
                     onItemSelected={this.onPersonSelected}
-                            getData={this.swapiService.getAllPlanets} />
+                            getData={this.swapiService.getAllPlanets} 
+                            renderItem={item => (<span>{item.name} <button>!</button></span>)}
+                            />
                 </div>
                 <div className="col-md-6">
                     <PersonDetails personId={this.state.selectedPerson} />
@@ -65,6 +67,7 @@ export default class App extends Component{
                         <ItemList
                             onItemSelected={this.onPersonSelected}
                             getData={this.swapiService.getAllStarships}
+                            renderItem={item => item.name}
                         />
                     </div>
                     <div className="col-md-6">
