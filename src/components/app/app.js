72ed8fb055343759cb86e9018898cd636d84cff5
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-12-03 09:34:03 +0600

    Add Row component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
-
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorButton from '../error-button';
 import ErrorIndicator from "../error-indicator";
+import './app.css';
 import PeoplePage from "../people-page";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import SwapiService from "../../services/swapi-service";
 
-import './app.css';
+
+
+
 
 export default class App extends Component{
     swapiService = new SwapiService();
@@ -18,65 +20,56 @@ export default class App extends Component{
         showRandomPlanet: true,
         hasError: false
     };
-
-
     toggleRandomPlanet = () => {
         this.setState((state)=> {
             return { showRandomPlanet: !state.showRandomPlanet }
         })
     };
-
-
     componentDidCatch() {
         this.setState({ hasError: true });
     }
-
-
     render() {
         if(this.state.hasError) return <ErrorIndicator />;
-
         const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-    return (
-        <div className='container-fluid'>
-            <div className="row">
-                <div className="col-12">
-                    <Header/>
-                    {planet}
-                    <button className="toggle-planet btn btn-warning btn-lg" onClick={this.toggleRandomPlanet}>
+        return (
+            <div className='container-fluid'>
+                <div className="row">
+                    <div className="col-12">
+                        <Header/>
+                        {planet}
+                        <button className="toggle-planet btn btn-warning btn-lg" onClick={this.toggleRandomPlanet}>
                             Toggle Random Planet
                         </button>
-                    <ErrorButton />
+                        <ErrorButton />
+                    </div>
                 </div>
-            </div>
-            <PeoplePage />
-            <div className="row">
-                <div className="col-md-6">
-                    <ItemList
-                    onItemSelected={this.onPersonSelected}
-                            getData={this.swapiService.getAllPlanets} 
-                            renderItem={item => (<span>{item.name} <button>!</button></span>)}
-                            />
-                </div>
-                <div className="col-md-6">
-                    <PersonDetails personId={this.state.selectedPerson} />
-                </div>
-            </div>
+                <PeoplePage />
 
-            <div className="row">
+                <div className="row">
                     <div className="col-md-6">
                         <ItemList
                             onItemSelected={this.onPersonSelected}
-                            getData={this.swapiService.getAllStarships}
-                            renderItem={item => item.name}
+                            getData={this.swapiService.getAllPlanets}
+                            renderItem={  (item) => <span>{item.name} <button>!</button></span>}
                         />
                     </div>
                     <div className="col-md-6">
                         <PersonDetails personId={this.state.selectedPerson} />
                     </div>
                 </div>
-            
-        </div>
-    );
+                <div className="row">
+                    <div className="col-md-6">
+                        <ItemList
+                            onItemSelected={this.onPersonSelected}
+                            getData={this.swapiService.getAllStarships}
+                            renderItem={  ({ name, gender, birthYear }) => `${name} 5`}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson} />
+                    </div>
+                </div>
+            </div>
+        );
     }
 };
-
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -2,16 +2,12 @@ import React, {Component} from 'react';
 import Spinner from "../spinner";
 import SwapiService from '../../services/swapi-service';
 import './item-list.css';
-
 export default class ItemList extends Component {
-
     state = {
         itemList: null
     };
-
     componentDidMount() {
         const {getData} = this.props;
-
         getData()
             .then( (itemList) => {
                 this.setState({
@@ -20,13 +16,11 @@ export default class ItemList extends Component {
             })
     }
 
-    renderItems = (arr) => {
+    renderItems(arr) {
         return arr.map((item) => {
             const {id} = item;
-
-            if(!this.props.renderItem) debugger;
-
             const label = this.props.renderItem(item);
+
             return (
                 <li
                     className="list-group-item"
@@ -41,7 +35,6 @@ export default class ItemList extends Component {
     render() {
         const {itemList} = this.state;
         if(!itemList) return <Spinner />;
-
         const items = this.renderItems(itemList);
         return (
             <ul className="item-list list-group">
