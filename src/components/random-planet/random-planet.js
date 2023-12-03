14a23a575fdb4e4f9a26f3f17615cad32e4c3fbc
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-12-03 08:44:08 +0600

    Code refactoring

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import SwapiService from "../../services/swapi-service";
-import Spinner from '../spinner'
+import Spinner from '../spinner';
 import ErrorIndicator from "../error-indicator";
 import './random-planet.css';
 
@@ -14,19 +14,19 @@ export default class RandomPlanet extends Component {
         error: false
     };
 
-    constructor() {
-        super();
+    componentDidMount() {
         this.updatePlanet();
+        this.interval = setInterval( this.updatePlanet, 60000 )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
     }
 
     onPlanetLoaded = (planet) => {
-        this.setState({planet, loading: false})
+        this.setState({planet, loading: false, error: false});
     };
 
-    updatePlanet() {
-        const id = Math.floor(Math.random() * 25 + 2);
-        this.swapiService.getPlanet(id).then(this.onPlanetLoaded).catch(this.onError)
-    }
 
     onError =  (err) => {
         this.setState(
@@ -36,6 +36,16 @@ export default class RandomPlanet extends Component {
             }
         )
     }
+
+    updatePlanet = () => {
+        const id = Math.floor( Math.random() * 17 ) + 3;
+
+        this.swapiService
+            .getPlanet(id)
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
+    };
+
     render() {
 
         const { planet, loading, error} = this.state;
@@ -62,7 +72,7 @@ const PlanetView = ({planet}) => {
     return (
         <React.Fragment>
             <img className="planet-image"
-                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet"/>
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
             <div>
                 <h4>{name}</h4>
                 <ul className="list-group list-group-flush">
